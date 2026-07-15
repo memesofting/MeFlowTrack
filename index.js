25ec8f41f2dfe10ea184103ee23c1a31afd587d9
memesofting <bibitayoadekunle00@gmail.com>
2026-07-15 21:40:44 +0100

    root and health endpoints

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello, world");
+    res.send({
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    });
 });
+
+app.get('/health', (req, res) => {
+    res.send({
+        "status": "ok"
+    })
+})
 
 app.listen(port, () => {
     console.log(`To-do app listening on port ${port}`)
