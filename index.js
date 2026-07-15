47970a814f8706bc3060329f02ec1720769e25de
memesofting <bibitayoadekunle00@gmail.com>
2026-07-15 22:26:44 +0100

    read endpoints with 404

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+    {
+        "id": 1,
+        "title": "Clear desk",
+        "done": true
+    },
+    {
+        "id": 2,
+        "title": "Clean the room",
+        "done": false
+    },
+    {
+        "id": 3,
+        "title": "Close all windows",
+        "done": true
+    }
+]
+
 app.get('/', (req, res) => {
     res.send({
         "name": "Task API",
@@ -16,6 +34,23 @@ app.get('/health', (req, res) => {
         "status": "ok"
     })
 })
+
+app.get('/tasks', (req, res) => {
+    res.send(tasks)
+})
+
+const getTaskById = async (req, res) => {
+    const { id } = req.params
+    const searchTask = tasks.find(task => task.id == id)
+    if (!searchTask) {
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        })
+    }
+    return res.send(searchTask)
+}
+
+app.get('/tasks/:id', getTaskById)
 
 app.listen(port, () => {
     console.log(`To-do app listening on port ${port}`)
